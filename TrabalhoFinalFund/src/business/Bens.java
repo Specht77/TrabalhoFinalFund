f6580596e4b8d793fafe1cc1baf3a30833c21f18
Specht77 <andreas.specht@gmail.com>
2017-06-06 22:14:30 -0300

    Criacao de algumas classes

---
+++ TrabalhoFinalFund/src/business/Bens.java
@@ -0,0 +1,13 @@
+package business;
+
+public class Bens {
+
+	private String descricaoBreve;
+	private String descricaoCompleta;
+	private String categoria;
+	
+	public Bens(String descricaoBreve, String descricaoCompleta, String categoria){
+		this.descricaoBreve = descricaoBreve;
+		
+	}
+}
---
+++ TrabalhoFinalFund/src/business/Usuario.java
@@ -0,0 +1,41 @@
+package business;
+
+public class Usuario {
+	
+	private String nome;
+	private int cpfcnpj;
+	private String email;
+	
+	public Usuario(String nome, int cpfcnpj, String email){
+		this.nome = nome;
+		this.cpfcnpj = cpfcnpj;
+		this.email = email;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setCpfcnpj(int cpfcnpj) {
+		this.cpfcnpj = cpfcnpj;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome(){
+		return nome;
+	}
+	
+	public int getCpfCnpj(){
+		return cpfcnpj;
+	}
+	
+	public String email(){
+		return email;
+	}
+	
+
+}
+
