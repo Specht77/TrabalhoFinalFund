b9476570f46788ab487479ebe42fd5cb55153ea4
Specht77 <andreas.specht@gmail.com>
2017-06-13 21:25:06 -0300

    Criação de variáveis e classes na classe Leilao, Criacao da classe Lote

---
+++ TrabalhoFinalFund/src/business/Lote.java
@@ -0,0 +1,22 @@
+package business;
+
+public class Lote {
+	
+	private int valorMinimo;
+	
+	public Lote(int valorMinimo){
+		this.valorMinimo = valorMinimo;
+		
+	}
+	
+	
+	
+	public void setValorMinimo(int valorMinimo){
+		this.valorMinimo = valorMinimo;
+	}
+	
+	public int getValorMinimo(){
+		return valorMinimo;
+	}
+
+}
